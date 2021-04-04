91bdc2c036786435d6c7442088a2e6f641cbbf46
Rotten Apple <Rotten_Apple@homes-Mac-mini.local>
2021-04-05 00:48:10 +0800

    rechore: 重构新歌推荐模块

--- src/components/recommandSongList/recommandSongList.vue
+++ src/components/recommandSongList/recommandSongList.vue
@@ -1,19 +1,21 @@
 <template>
     <div id="recommandSongList">
-        <div class="box">
+        <div class="box" v-if="songListData.singerName">
         <span style="font-size:.5rem">根据“{{songListData.singerName}}”推荐</span>
         <div>
         <img class="logo" src="@/assets/images/video1.png"/>
-        <span>播放</span>
+        <span v-if="songListData.singerName">播放</span>
         </div>
         </div>
         <ul class="box1">
             <li v-for="(items,index) in songListData.listData" :key="index">
                 <ul class="box2">
             <li v-for="(item,sindex) in items" :key="sindex">
-                <img class="pic" :src="item.blurPicUrl">
+                <img class="pic" v-if="item.blurPicUrl" :src="item.blurPicUrl">
+                <img class="pic" v-if="item.picUrl && !item.blurPicUrl" :src="item.picUrl">
                 <div class="message">
-                    <span>{{item.name}}-{{item.artists[0].name}}</span>
+                    <span v-if="item.artists">{{item.name}}-{{item.artists[0].name}}</span>
+                    <span v-if="!item.artists">{{item.name}}-{{item.song.artists[0].name}}</span>
                     <span>{{item.description}}</span>
                 </div>
                 <div class="el-icon-film"></div>
--- src/containers/menu/main/musicStore/musicStore.vue
+++ src/containers/menu/main/musicStore/musicStore.vue
@@ -1,71 +1,145 @@
 <template>
-    <div id="musicstore">
-      <!-- 海报 -->
-        <div class="song-block" style="width: 100%;top:2.6rem;height: 3.5rem;">
-          <banner :bannerData="bannerData"></banner>
-        </div>
-        <!-- 按钮 -->
-        <div class="song-block" style="height: 2rem;">
-          <div class="box" @click="goTo(1)">
-                <img src="@/assets/images/singger.png">
-                <span class="title">歌手</span>
+  <div id="musicstore">
+    <!-- 海报 -->
+    <div class="song-block" style="width: 100%; top: 2.6rem; height: 3.5rem">
+      <banner :bannerData="bannerData"></banner>
+    </div>
+    <!-- 按钮 -->
+    <div class="song-block" style="height: 2rem">
+      <div class="box" @click="goTo(1)">
+        <img src="@/assets/images/singger.png" />
+        <span class="title">歌手</span>
+      </div>
+      <div class="box" @click="openRanks">
+        <img src="@/assets/images/sole.png" />
+        <span class="title">排行</span>
+      </div>
+      <div class="box" @click="openItemize">
+        <img src="@/assets/images/rank.png" />
+        <span class="title">分类歌单</span>
+      </div>
+      <div class="box">
+        <img src="@/assets/images/radio.png" />
+        <span class="title">电台</span>
+      </div>
+      <div class="box">
+        <img src="@/assets/images/live.png" />
+        <span class="title">直播</span>
+      </div>
+    </div>
+    <!-- 编辑甄选 -->
+    <div class="song-block" style="height: 4rem">
+      <songlist :songListData="speSongList"></songlist>
+    </div>
+    <!-- 热门话题 -->
+    <div class="song-block" style="height: 0.8rem">
+      <tags :isTags="true" :list="tag"></tags>
+    </div>
+    <!-- 新歌推荐 -->
+    <div class="song-block" style="width: 90%; height: 5.6rem">
+      <el-tabs>
+        <el-tab-pane>
+          <span slot="label">
+            <span>新歌</span>
+          </span>
+          <recommandSongList :songListData="newSong"></recommandSongList>
+        </el-tab-pane>
+        <el-tab-pane>
+          <span slot="label">
+            <span>新碟</span>
+          </span>
+          <recommandSongList :songListData="newAblum"></recommandSongList>
+        </el-tab-pane>
+        <el-tab-pane>
+          <span slot="label">
+            <span>数字专辑</span>
+          </span>
+          <div id="numcd">
+            <img
+              style="
+                width: 3rem;
+                height: 3rem;
+                border-radius: 0.3rem;
+                position: absolute;
+                margin-top: 0.3rem;
+              "
+              :src="products.coverUrl"
+            />
+            <div
+              style="
+                width: 100%;
+                height: 3.5rem;
+                display: flex;
+                flex-direction: row;
+              "
+            >
+              <img
+                style="
+                  width: 3rem;
+                  height: 3rem;
+                  margin-top: 0.3rem;
+                  margin-left: 0.5rem;
+                "
+                src="@/assets/images/cd.png"
+              />
+              <div style="
+                    width: 5rem;
+                    height: 4rem;
+                    margin-top: .2rem;
+                    margin-left: .4rem;
+                    height: 3rem;
+                    display: flex;
+                    flex-direction: column;
+              ">
+                <span
+                  style="
+                    font-size: 0.5rem;
+                    font-family: Helvetica;
+                    color: black;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+                    overflow: hidden;
+                    margin-top: 0.1rem;
+                  "
+                  >{{ products.albumName }}</span
+                >
+                <span class="singger">{{ products.artistName }}</span>
+                <el-button round>立即支持</el-button>
+                <div class="sell">
+                  <span>已售</span><span>{{ products.saleNum }}</span
+                  ><span>万张</span>
+                </div>
+              </div>
             </div>
-          <div class="box" @click="openRanks">
-                <img src="@/assets/images/sole.png">
-                <span class="title">排行</span>
           </div>
-            <div class="box" @click="openItemize">
-                <img src="@/assets/images/rank.png">
-                <span class="title">分类歌单</span>
-            </div>
-            <div class="box">
-                <img src="@/assets/images/radio.png">
-                <span class="title">电台</span>
-            </div>
-            <div class="box">
-                <img src="@/assets/images/live.png">
-                <span class="title">直播</span>
-            </div>
-        </div>
-        <!-- 编辑甄选 -->
-        <div class="song-block" style="height: 4rem">
-          <songlist :songListData="speSongList" ></songlist>
-        </div>
-        <!-- -->
-        <div class="song-block" style="height: .8rem">
-          <tags :isTags="true" :list="tag"></tags>
-        </div>
-        <!-- -->
-        <div class="song-block" style="height: 5.6rem;margin-top: .4rem;">
-          <newsongs></newsongs>
-        </div>
-        <!-- -->
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+    <!--
         <div class="song-block" style="height: 4.5rem;margin-top: .3rem;">
           <rank></rank>
-        </div>
-        <!-- -->
+        </div> -->
+    <!--
         <div class="song-block" style="height: 6rem">
           <lives></lives>
-        </div>
-        <!-- -->
+        </div> -->
+    <!--
         <div class="song-block" style="height: 5.5rem">
           <classify></classify>
-        </div>
-        <!-- -->
+        </div> -->
+    <!--
         <div class="song-block" style="height: 5.4rem">
           <sole></sole>
-        </div>
-        <!-- -->
+        </div> -->
+    <!--
         <div class="song-block" style="height: 6rem">
           <musician></musician>
-        </div>
-        <!-- -->
-      <div class="bottom"></div>
-    </div>
+        </div> -->
+    <div class="bottom"></div>
+  </div>
 </template>
 <script>
 import special from '@/components/pages/first/musicstore/special/special'
-import newsongs from '@/components/pages/first/musicstore/newsongs/newsongs'
 import rank from '@/components/pages/first/musicstore/rank/rank'
 import lives from '@/components/pages/first/musicstore/lives/lives'
 import classify from '@/components/pages/first/musicstore/classify/classify'
@@ -76,11 +150,11 @@ import Banner from '../../../../components/banner/banner.vue'
 import axios from 'axios'
 import Tags from '../../../../components/tags/index.vue'
 import songlist from '../../../../components/songlist/songlist.vue'
+import recommandSongList from '../../../../components/recommandSongList/recommandSongList.vue'
 export default {
   name: 'musicstore',
   components: {
     special,
-    newsongs,
     rank,
     lives,
     classify,
@@ -89,7 +163,8 @@ export default {
     allranks,
     Banner,
     Tags,
-    songlist
+    songlist,
+    recommandSongList
   },
   data () {
     return {
@@ -100,6 +175,15 @@ export default {
       bannerData: {
         listData: []
       },
+      newSong: {
+        listData: []
+      },
+      newAblum: {
+        listData: []
+      },
+      products: {
+        coverUrl: undefined
+      },
       speSongList: {
         listData: [],
         listName: '编辑甄选'
@@ -108,7 +192,7 @@ export default {
   },
   methods: {
     goTo (data) {
-      this.$router.push({path: '/singerList'})
+      this.$router.push({ path: '/singerList' })
     },
     openSinger () {
       this.isSinger = true
@@ -142,11 +226,9 @@ export default {
         withCredentials: true
       })
       hots = res.data.result.hots
-      console.log(hots)
       for (var i in hots) {
         this.tag.push(hots[i].first)
       }
-      console.log(this.tag)
     },
     async getRecommendSong () {
       const res = await axios({
@@ -154,17 +236,48 @@ export default {
         withCredentials: true
       })
       this.speSongList.listData = res.data.playlists
-      console.log(res)
+    },
+    async getNewSong () {
+      let albums = []
+      const res = await axios({
+        url: `http://localhost:3000/personalized/newsong?limit=9`,
+        withCredentials: true
+      })
+      for (let i = 0; i < res.data.result.length;) {
+        albums.push(res.data.result.slice(i, (i += 3)))
+      }
+      this.newSong.listData = albums
+    },
+    async getNewNumberAblum () {
+      const res = await axios({
+        url: `http://localhost:3000/album/list?limit=1`,
+        withCredentials: true
+      })
+      this.products = res.data.products[0]
+    },
+    async getNewAblum () {
+      let albums = []
+      const res = await axios({
+        url: `http://localhost:3000/album/new?limit=9`,
+        withCredentials: true
+      })
+      for (let i = 0; i < res.data.albums.length;) {
+        albums.push(res.data.albums.slice(i, i += 3))
+      }
+      this.newAblum.listData = albums
     }
   },
   mounted () {
     this.getBanner()
     this.getHotTopic()
     this.getRecommendSong()
+    this.getNewSong()
+    this.getNewNumberAblum()
+    this.getNewAblum()
   }
 }
 </script>
 <style scoped>
-@import './index.css';
+@import "./index.css";
 
 </style>
